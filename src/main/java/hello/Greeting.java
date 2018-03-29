496002631704f3e786673d1c2614b9bdf729ac5e
Ryan Chalmers <ryan.chalmers@mail.mcgill.ca>
2018-03-29 17:04:36 -0400

    testing asciidoc connection to github

--- src/main/java/hello/Greeting.java
+++ src/main/java/hello/Greeting.java
@@ -4,15 +4,18 @@ public class Greeting {
 
   private final long id;
   private final String content;
-
+  
+  
   public Greeting(long id, String content) {
     this.id = id;
     this.content = content;
   }
-
+  
+  //tag::getID[]
   public long getId() {
     return id;
   }
+  //end::getID[]
 
   public String getContent() {
     return content;
